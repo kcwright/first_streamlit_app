3eace933927941d32de06be971fcd54fd457c175
kcwright <kippcwright@gmail.com>
2022-12-27 17:01:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('":heart:"My parents New Healthy Diner')
+streamlit.title(:heart:"'My parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
